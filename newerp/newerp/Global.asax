57c8a46479c4a6ac3c2340d7331255d2d3232d77
ganeshchavandev@outlook.com <ganeshchavandev@outlook.com>
2019-05-14 14:40:03 +0530

    commitpoonam

---
+++ newerp/newerp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="newerp.Global" Language="C#" %>
---
+++ newerp/newerp/Site.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace newerp
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
